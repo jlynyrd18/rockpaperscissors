eff4bb34cf92e11ceabf726728b136f7a430edd7
Jordan Lenard <leonard_2010@live.com>
2022-08-11 21:48:14 -0500

    got function to work and print

--- rpsconsole.js
+++ rpsconsole.js
@@ -1,12 +1,17 @@
-function (getComputerChoice) {
-    choice = Math.floor(Math.random() * 10);
+//First a function that will produce a random choice from 3 choices
+//trying to use numbers to tie to choice and assign
+
+function getComputerChoice() {
+    var choice = Math.floor(Math.random() * 10);
     if (choice <= 3) {
-        choice = "Rock";
+        choice = 1;
     }
     else if (choice <= 6) {
-        choice = "Paper";
+        choice = 2;
     }
     else {
-        choice = "scissors";
+        choice = 3;
     }
+    return choice;
 }
+console.log(getComputerChoice());
